08cff07c9cda7d3686ad6d8c62d86706d3a3ad9c
mattan5271 <mattan.system.engineering@gmail.com>
2022-06-08 17:20:33 +0900

    ⚡️ Todo作成フォーム実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,49 @@
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
 import axios, { AxiosError, AxiosResponse } from "axios";
 
-const Home: NextPage = () => {
-  const [string, setString] = useState<string>("");
+import { Button, Center, Container, Textarea, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
-  useEffect(() => {
+const Home: NextPage = () => {
+  const form = useForm({
+    initialValues: {
+      title: "",
+      description: "",
+    },
+
+    validate: {
+      description: (value) => (value.length > 50 ? "説明文は50文字以内にしてください" : null),
+    },
+  });
+  type FormValues = typeof form.values;
+
+  const handleSubmit = (formValues: FormValues): void => {
+    console.log(formValues);
     axios
-      .get(`${process.env.NEXT_PUBLIC_API_URL}`)
+      .post(`${process.env.NEXT_PUBLIC_API_URL}/todos`, formValues)
       .then((res: AxiosResponse) => {
-        setString(res.data);
+        console.log(res);
       })
       .catch((err: AxiosError<{ error: string }>) => {
         console.log(err);
       });
-  }, []);
+  };
 
-  return <h1>{string}</h1>;
+  // useEffect(() => {}, []);
+
+  return (
+    <Container>
+      <form onSubmit={form.onSubmit((formValues: FormValues) => handleSubmit(formValues))}>
+        <TextInput required label="タイトル" {...form.getInputProps("title")} />
+        <Textarea label="説明文" description="50文字以下で入力してください" mt="md" {...form.getInputProps("description")} />
+        <Center mt="md">
+          <Button type="submit" color="green">
+            Todoを登録
+          </Button>
+        </Center>
+      </form>
+    </Container>
+  );
 };
 
 export default Home;
