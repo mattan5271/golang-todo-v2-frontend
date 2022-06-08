31589edad4c8172351d43d21a647fe8da69dfbc8
mattan5271 <mattan.system.engineering@gmail.com>
2022-06-08 17:21:08 +0900

    ⚡️ Todo一覧表示実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import axios, { AxiosError, AxiosResponse } from "axios";
 
-import { Button, Center, Container, Textarea, TextInput } from "@mantine/core";
+import { Button, Center, Container, Table, Textarea, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 
+export type Todo = {
+  id: number;
+  title: string;
+  description: string;
+};
+
 const Home: NextPage = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
   const form = useForm({
     initialValues: {
       title: "",
@@ -18,7 +26,6 @@ const Home: NextPage = () => {
   type FormValues = typeof form.values;
 
   const handleSubmit = (formValues: FormValues): void => {
-    console.log(formValues);
     axios
       .post(`${process.env.NEXT_PUBLIC_API_URL}/todos`, formValues)
       .then((res: AxiosResponse) => {
@@ -29,19 +36,45 @@ const Home: NextPage = () => {
       });
   };
 
-  // useEffect(() => {}, []);
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_API_URL}/todos`)
+      .then((res: AxiosResponse) => {
+        setTodos(res.data);
+      })
+      .catch((err: AxiosError<{ error: string }>) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <Container>
       <form onSubmit={form.onSubmit((formValues: FormValues) => handleSubmit(formValues))}>
         <TextInput required label="タイトル" {...form.getInputProps("title")} />
         <Textarea label="説明文" description="50文字以下で入力してください" mt="md" {...form.getInputProps("description")} />
-        <Center mt="md">
+        <Center my="md">
           <Button type="submit" color="green">
             Todoを登録
           </Button>
         </Center>
       </form>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>タイトル</th>
+            <th>説明文</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo: Todo) => (
+            <tr key={todo.id}>
+              <td>{todo.title}</td>
+              <td>{todo.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Container>
   );
 };
